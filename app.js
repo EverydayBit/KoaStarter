1d87bac03b06b65e5d16080d4a2bc8e63dd32598
EverydayBit <dsr931108+gitblog@gmail.com[core]>
2016-10-29 20:12:49 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,96 @@
+'use strict';
+
+require('babel-polyfill');
+
+var _koa = require('koa');
+
+var _koa2 = _interopRequireDefault(_koa);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+var app = new _koa2.default();
+
+app.use(function () {
+    var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(ctx, next) {
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+            while (1) {
+                switch (_context.prev = _context.next) {
+                    case 0:
+                        // Called before calling the next middleware 
+                        console.log('going down');
+                        _context.next = 3;
+                        return next();
+
+                    case 3:
+
+                        // Called after calling the next middleware
+                        console.log('going more up');
+
+                    case 4:
+                    case 'end':
+                        return _context.stop();
+                }
+            }
+        }, _callee, undefined);
+    }));
+
+    return function (_x, _x2) {
+        return _ref.apply(this, arguments);
+    };
+}());
+
+app.use(function () {
+    var _ref2 = _asyncToGenerator(regeneratorRuntime.mark(function _callee2(ctx, next) {
+        return regeneratorRuntime.wrap(function _callee2$(_context2) {
+            while (1) {
+                switch (_context2.prev = _context2.next) {
+                    case 0:
+                        // Called before calling the next middleware 
+                        console.log('going more down');
+                        _context2.next = 3;
+                        return next();
+
+                    case 3:
+
+                        // Called after calling the next middleware
+                        console.log('going up');
+
+                    case 4:
+                    case 'end':
+                        return _context2.stop();
+                }
+            }
+        }, _callee2, undefined);
+    }));
+
+    return function (_x3, _x4) {
+        return _ref2.apply(this, arguments);
+    };
+}());
+
+app.use(function () {
+    var _ref3 = _asyncToGenerator(regeneratorRuntime.mark(function _callee3(ctx) {
+        return regeneratorRuntime.wrap(function _callee3$(_context3) {
+            while (1) {
+                switch (_context3.prev = _context3.next) {
+                    case 0:
+                        // no more next(); 
+                        // head back up the stack
+                        ctx.body = 'Hello world';
+
+                    case 1:
+                    case 'end':
+                        return _context3.stop();
+                }
+            }
+        }, _callee3, undefined);
+    }));
+
+    return function (_x5) {
+        return _ref3.apply(this, arguments);
+    };
+}());
+
+app.listen(3000);
